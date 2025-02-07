83b3fed9877aff2e27e1ea7369bcbd5d9bb4dc1b
mahdikhashan <mahdikhashan1@gmail.com>
2025-02-07 16:10:39 +0100

    lint nix files

--- flake.nix
+++ flake.nix
@@ -1,8 +1,6 @@
 {
   description = "my nix setup";
-  inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-  };
+  inputs = { nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable"; };
   outputs = { self, nixpkgs }: {
     devShells.aarch64-darwin = {
       default = import ./python { inherit nixpkgs; };
--- python/default.nix
+++ python/default.nix
@@ -1,12 +1,8 @@
 { nixpkgs }:
 
-let
-  pkgs = nixpkgs.legacyPackages.aarch64-darwin;
-in
-pkgs.mkShell {
-  buildInputs = with pkgs; [
-    python311
-  ];
+let pkgs = nixpkgs.legacyPackages.aarch64-darwin;
+in pkgs.mkShell {
+  buildInputs = with pkgs; [ python311 ];
 
   shellHook = ''
     echo "🐍 Base Python environment activated!"
